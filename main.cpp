3a1c1194c9cfce339cd4f045ba3c9339fdb0c50a
Stephen Hara 10647 <stephen.hara@tri-ad.global>
2022-03-25 14:51:19 +0900

    code: add a bad virtual function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+class Base {
+private:
+    // pure virtual member function used for initialization of derived classes.
+    virtual void construct() = 0;
+public:
+    Base() {
+        // wrong: the virtual table of `Derived` has not been initialized yet. So this
+        // call will resolve to `Base::construct`, which cannot be called as it is a pure
+        // virtual function.
+        construct();
+    }
+};
+
 int main(int argc, char** argv) {
 	int *p = new int();
 	delete p;
