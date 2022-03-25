384093ce9ab632689fede5300b30dd12ce030e3e
Stephen Hara 10647 <stephen.hara@tri-ad.global>
2022-03-25 14:38:24 +0900

    code: try delete same pointer twice

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
-	int i = 0;
-	if (i % 2 == 0) {
-		std::cout << "i is even" << std::endl;
-	} else {
-	}
+	int *p = new int();
+	delete p;
+	delete p;
 
 	return 0;
 }
